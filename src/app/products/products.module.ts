29f9bf939ebf166718fdb9b58de0a8f0960c09bf
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-07-17 12:07:21 +0300

    add standalone component

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductComponent
+  ]
+})
+export class ProductsModule { }
