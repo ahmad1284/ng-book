8715cac140def7009ca2cc02ca0aaad2341b33c4
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-07-18 11:22:15 +0300

    feat v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Learning Angular';
+  title = 'my-app-v2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ProductsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
